1fce2cbecaecc60a7a62a2491ebc2caa81b1347b
josue eduardo <josue.ferrufino.soft@gmail.com>
2018-10-30 10:26:00 -0400

    reversion

---
+++ TheDuckGame/Ocp/MuteFlyDuck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheDuckGame.Ocp
+{
+    
+    class MuteFlyDuck : IDucksOcp
+    
+    {
+
+        private IFlyBehavior flyBehavior;
+
+        public MuteFlyDuck(IFlyBehavior behavior) {
+            this.flyBehavior = behavior;
+        }
+
+        public override void PerformQuack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PerforFly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Swim()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Display()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
